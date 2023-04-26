92bc5fab127b2c4061e5722234436f8313ae432c
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-04-26 18:40:36 +0530

    refractor: format code- handle uncaught errors

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,44 @@
-const app = require('./app');
-require('dotenv').config({ path: './config.env' });
-const mongoose = require('mongoose');
+const app = require("./app");
+require("dotenv").config({ path: "./config.env" });
+const mongoose = require("mongoose");
+const process = require("process");
 
 //connect to mongo database
-const connect = mongoose.connect(process.env.DB_CONNECTION_STRING.replace('<password>', process.env.DB_PASSWORD), {
-    useNewUrlParser: true
-}).then(() => console.log('-----DB connection successful!-----')).catch(err => console.log(err));
+mongoose
+  .connect(
+    process.env.DB_CONNECTION_STRING.replace(
+      "<password>",
+      process.env.DB_PASSWORD
+    ),
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => {
+    console.log("-----DB connection successful!-----");
+    console.log(`running on ${process.env.NODE_ENV} environment.......`);
+  })
+  .catch((err) => console.log(err));
 
 //create express server
 const port = process.env.PORT || 3000;
-const host = process.env.HOST || '127.0.0.1';
+const host = process.env.HOST || "127.0.0.1";
 const server = app.listen(port, host, () => {
-    console.log('server is now running on port ' + port + '...');
-    
+  console.log("server is now running on port " + port + "...");
 });
 
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("unhandled rejection, shutting down");
+  server.close(() => {
+    process.exit(1);
+  });
 
-
+  process.on("uncaughtException", (err) => {
+    console.log(err.name, err.message);
+    console.log("uncaught exception, shutting down");
+    server.close(() => {
+      process.exit(1);
+    });
+  });
+});
