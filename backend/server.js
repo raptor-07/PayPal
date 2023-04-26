b44475c78c84d550130779082ca459699c724943
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-04-26 18:20:42 +0530

    feat: create express server

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,13 @@ const connect = mongoose.connect(process.env.DB_CONNECTION_STRING.replace('<pass
     useNewUrlParser: true
 }).then(() => console.log('-----DB connection successful!-----')).catch(err => console.log(err));
 
+//create express server
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || '127.0.0.1';
+const server = app.listen(port, host, () => {
+    console.log('server is now running on port ' + port + '...');
+    
+});
 
 
 
