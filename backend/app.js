e2a4b6853e92ca0201109d808054ad3ee904106c
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-04-27 13:00:08 +0530

    build: add morgan, jsonparser

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const authRouter = require("./routers/authRouter");
 const userRouter = require("./routers/userRouter");
 const groupRouter = require("./routers/groupRouter");
 
 //util 3PMiddleware
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 //middlewares for common routes
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
