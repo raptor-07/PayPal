a34771c178cdbeec5ee10d086775ffd99f474369
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-04-27 13:03:25 +0530

    feat: add funcitonality for /signup route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,29 +1,39 @@
-// const User = require("../models/userModel");
+const User = require("../models/userModel");
+const jwtSigner = require("../utils/jwtSigner");
 
-const signup = (req, res) => {
-    res.send("signup");
+const signup = async (req, res) => {
+  const newUser = await new User(req.body).save();
+  const data = await User.findById(newUser.id).lean();
+  const token = jwtSigner.signToken(data);
+  res.status(201).json({
+    status: "success",
+    data: {
+      token,
+    },
+  });
 };
+
 const login = (req, res) => {
-    res.send("login");
+  res.send("login");
 };
 const logout = (req, res) => {
-    res.send("logout");
+  res.send("logout");
 };
 const forgotPassword = (req, res) => {
-    res.send("forgotPassword");
+  res.send("forgotPassword");
 };
 const resetPassword = (req, res) => {
-    res.send("resetPassword");
+  res.send("resetPassword");
 };
 const updateEmail = (req, res) => {
-    res.send("updateEmail");
+  res.send("updateEmail");
 };
 
 module.exports = {
-    signup,
-    login,
-    logout,
-    forgotPassword,
-    resetPassword,
-    updateEmail
+  signup,
+  login,
+  logout,
+  forgotPassword,
+  resetPassword,
+  updateEmail,
 };
